e7edf2edbf285bf1062b47825ee39911fe3e9a55
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-04-30 20:04:54 +0000

    automatically backed up by learn

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -4,7 +4,11 @@ export default class RegistrationForm extends React.Component {
   render () {
     return (
       <div>
-        
+      <form>
+        <input type='text' />
+        <input type='password' />
+        <button type='submit' />
+      </form>
       </div>
     )
   }
